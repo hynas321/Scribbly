f14939ef836f91091da6929265a3ac28c9c17346
Piotr <hynas321@gmail.com>
2023-04-24 23:26:47 +0200

    Add black color to color pallete

--- react-app/src/components/Canvas.tsx
+++ react-app/src/components/Canvas.tsx
@@ -4,14 +4,22 @@ import { CirclePicker } from "react-color"
 import Button from "./Button";
 import ProgressBar from "./ProgressBar";
 import { GameHubContext } from "../Context/GameHubContext";
+import * as material from 'material-colors'
 
 interface CanvasProps {
   progressBarProperties: ProgressProperties
 }
-
 function Canvas({progressBarProperties}: CanvasProps) {
   const hub = useContext(GameHubContext);
   const gameHash = "TestGameHash"; //temporary
+
+  const circlePickerColors = [material.black, material.red['500'],
+    material.pink['500'], material.purple['500'], material.deepPurple['500'],
+    material.indigo['500'], material.blue['500'], material.lightBlue['500'],
+    material.green['500'], material.lightGreen['500'], material.lime['500'],
+    material.yellow['500'], material.amber['500'], material.orange['500'],
+    material.deepOrange['500'], material.brown['500'], material.blueGrey['500']
+  ]
 
   const [color, setColor] = useState("#000000");
   const { canvasRef, onMouseDown, clearCanvas } = useDraw(draw, hub, gameHash, color);
@@ -53,11 +61,14 @@ function Canvas({progressBarProperties}: CanvasProps) {
       />
       <div>
       </div>
-      <CirclePicker
-        color={color}
-        width="100"
-        onChange={(e) => setColor(e.hex)}
-      />
+      <div className="d-flex justify-content-center">
+        <CirclePicker
+          color={color}
+          width="100"
+          onChange={(e) => setColor(e.hex)}
+          colors={circlePickerColors}
+        />
+      </div>
       <Button 
         text={"Clear canvas"}
         active={true}
